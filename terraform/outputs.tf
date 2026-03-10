6efe09acee70d00c713697a4210f3189e1e489d7
keerthirajamani <keerthyrajamani@gmail.com>
2026-03-10 23:35:37 +0530

    bugfix_2

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,6 +3,10 @@ output "ecr_repository_url" {
   value       = module.ecr.repository_url
 }
 
+output "ecr_repository_name" {
+  value = var.ecr_repository_name
+}
+
 output "lambda_function_name" {
   description = "Lambda function name"
   value       = module.lambda.function_name
