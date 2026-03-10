05c771ccd32df896d99696c6ac50802dab2f036b
keerthirajamani <keerthyrajamani@gmail.com>
2026-03-10 23:59:20 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,6 @@ provider "aws" {
 module "ecr" {
   source          = "./modules/ecr"
   repository_name = var.ecr_repository_name
-  tags            = local.common_tags
 }
 
 # ──────────────────────────────────────────────
