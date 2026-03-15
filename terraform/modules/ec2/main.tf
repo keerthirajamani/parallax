1622f8bf3e6e16914865c60a766305c030eb0b19
keerthirajamani <keerthyrajamani@gmail.com>
2026-03-15 19:03:18 +0530

    comments

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -1,5 +1,4 @@
 resource "aws_security_group" "ec2_sg" {
-
   vpc_id = var.vpc_id
   ingress {
     description = "SSH access"
@@ -8,45 +7,31 @@ resource "aws_security_group" "ec2_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]   # Better to restrict to your IP
   }
-
   egress {
-
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-
   }
 
 }
 
 data "aws_ami" "amazon_linux" {
-
   most_recent = true
   owners      = ["amazon"]
-
   filter {
-
     name   = "name"
     values = ["al2023-ami-2023.*-x86_64"]
-
   }
-
 }
 
 resource "aws_instance" "signal_engine" {
-
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
-
   subnet_id = var.subnet_id
-
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
-
   iam_instance_profile = var.instance_profile
-
   key_name = var.key_name
-
   tags = {
     Name        = "parallax-${var.environment}-signal-engine"
     Environment = var.environment
