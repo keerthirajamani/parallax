7f654a16088838054284ec4d24a3ccc5dd56f535
keerthirajamani <keerthyrajamani@gmail.com>
2026-03-15 17:53:39 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -9,3 +9,11 @@ output "ec2_instance_id" {
 output "lambda_function_name" {
   value = module.lambda.lambda_name
 }
+
+output "lambda_function_arn" {
+  value = module.lambda.lambda_arn
+}
+
+output "ec2_public_ip" {
+  value = module.ec2.public_ip
+}
